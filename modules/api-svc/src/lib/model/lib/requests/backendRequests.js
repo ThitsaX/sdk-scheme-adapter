347c3ad1bb474703b3c5a1d44e174dbac6935990
sandi <102009066+sandisoee@users.noreply.github.com>
2025-06-27 03:59:27 +0630

    Update backendRequests.js

--- modules/api-svc/src/lib/model/lib/requests/backendRequests.js
+++ modules/api-svc/src/lib/model/lib/requests/backendRequests.js
@@ -269,7 +269,7 @@ class BackendRequests {
     async sendRequest(reqOptions) {
         try {
             this.logger.isVerboseEnabled && this.logger.push({ reqOptions }).verbose(`Executing HTTP ${reqOptions?.method}...`);
-            const res = await this.requester.sendRequest({ ...reqOptions, agent: this.agent });
+            const res = await this.requester.sendRequest({ ...reqOptions, agent: this.agent,responseType: 'json' });
             console.log("res:",res);
 
             const data = (res.headers['content-length'] === '0' || res.statusCode === 204)
