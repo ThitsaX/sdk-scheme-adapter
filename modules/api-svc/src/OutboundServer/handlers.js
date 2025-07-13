03f3b755dceb1a3d4abc15417c64de7726324bac
sandi <102009066+sandisoee@users.noreply.github.com>
2025-07-14 01:27:27 +0630

    Update handlers.js

--- modules/api-svc/src/OutboundServer/handlers.js
+++ modules/api-svc/src/OutboundServer/handlers.js
@@ -183,7 +183,7 @@ const getTransfers = async (ctx) => {
         } catch (loadErr) {
             console.error('Transfer not found:', loadErr);
 
-            ctx.response.status =  ReturnCodes.OK.CODE;
+            ctx.response.status =  ReturnCodes.NOTFOUND.CODE;
             ctx.response.body = {
                 statusCode: "3208",
                 message: "Transfer ID not found"
