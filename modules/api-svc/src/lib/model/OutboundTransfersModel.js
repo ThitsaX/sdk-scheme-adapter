3c9d74286c3b9db14f622f978729abe94d880ddf
SithuKyaw-Thitsaworks <sithu.kyaw@thitsaworks.com>
2026-03-23 17:45:00 +0700

    chore: removing the codeline writing transferUI_out key

--- modules/api-svc/src/lib/model/OutboundTransfersModel.js
+++ modules/api-svc/src/lib/model/OutboundTransfersModel.js
@@ -1327,7 +1327,7 @@ class OutboundTransfersModel {
             const modifiedData = this._modifyDataForUi(this.data);
             // save to a UI key, using a modifiedData, as we don't want any side effects to happen on original data
             // No ttl set as it will persist throughout the session
-            await this._cache.set(`transferUI_out_${this.data.transferId}`, modifiedData);
+            //await this._cache.set(`transferUI_out_${this.data.transferId}`, modifiedData);
             this._logger.isDebugEnabled && this._logger.push({ res }).debug('Persisted transfer model in cache');
         }
         catch (err) {
